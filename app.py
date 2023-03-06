c2fb82e1e428748e4627ccf230d6811b4afd9b8f
Rick <coastk05396@gmail.com>
2023-03-06 11:19:23 +0800

    update

--- app.py
+++ app.py
@@ -7,10 +7,12 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-def create_app():
-    app = Flask(__name__)
-    client = MongoClient(os.environ.get("MONGODB_URI"))
-    app.db = client.get_default_database()
+# def create_app():
+app = Flask(__name__)
+client = MongoClient(os.environ.get("MONGODB_URI"))
+app.db = client.get_default_database()
 
-    app.register_blueprint(pages)
-    return app
+app.register_blueprint(pages)
+# return app
+if __name__ == "__main__":
+    app.run
